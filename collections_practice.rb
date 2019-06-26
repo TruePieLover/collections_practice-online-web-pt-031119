af46cac803c481010b42e2f2dbe07e1edd5538f0
Brandon Teich <brandonteich123@gmail.com>
2019-06-26 15:17:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,9 +32,9 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  array.find_all do |word|
-    word[0] == "a"
-  end
+  array.find_all do |w|
+    w[0] == "a"
+end
 
   # using select method
     # array.select do |word|
